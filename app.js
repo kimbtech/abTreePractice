a31e4065f06a9a8b757f55c7800048e2c35c0c8a
KIMB-technologies <info@kimb-technologies.eu>
2018-07-02 12:36:52 +0200

    All libs in repo

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ angular.module('d3', [])
       var scriptTag = $document[0].createElement('script');
       scriptTag.type = 'text/javascript';
       scriptTag.async = true;
-      scriptTag.src = 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js';
+      scriptTag.src = 'load/d3.min.js';
       scriptTag.onreadystatechange = function () {
         if (this.readyState == 'complete') { onScriptLoad(); }
       }
